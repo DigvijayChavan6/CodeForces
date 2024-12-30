1b7612e28c61a9a6a8f3d48779ae5aa2e1bee8fa
Digvijay Chavan <chavandigvijay9081@gmail.com>
2024-12-30 10:17:12 +0530

    B Level

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,5 @@
-#include<iostream>
-using namespace std;
-
-bool isLargeSum(string x){
-    int n=x.size();
-    if(x[0]!='1' || x[n-1]=='9')return false;
-    int i=1;
-    while(i<=n-2){
-        if(x[i] == '0')return false;
-        i++;
-    }
-    return true;
-}
-
-int main(){
-    long long t;
-    cin>>t;
-    while(t--){
-        string x;
-        cin>>x;
-        if(isLargeSum(x)){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }
-    }
-    return 0;
-}
+for(int i = 0; i < n; ++i){
+        //     char c;
+        //     cin >> c;
+        //     s += c;
+        // }
